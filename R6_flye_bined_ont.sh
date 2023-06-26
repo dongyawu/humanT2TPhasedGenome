e1488499063bb2fd8b78bc99efd31cb95688a209
dongyawu <31070209+dongyawu@users.noreply.github.com>
2023-06-26 09:51:18 +0800

    Update R6_flye_bined_ont.sh

--- R6_flye_bined_ont.sh
+++ R6_flye_bined_ont.sh
@@ -10,9 +10,9 @@
 date
 threads=20
 famID="C019-CHA-E19"
-yak2="/slurm/home/zju/zhanglab/wudongya/APG/NGS/${famID}-02/2update.yak"
-yak3="/slurm/home/zju/zhanglab/wudongya/APG/NGS/${famID}-03/3update.yak"
-dir="/slurm/home/zju/zhanglab/wudongya/APG/Nanopore/${famID}-01/binning_100k/haplotype"
+yak2="/YOUR_PATH/NGS/${famID}-02/2update.yak"
+yak3="/YOUR_PATH/NGS/${famID}-03/3update.yak"
+dir="/YOUR_PATH/Nanopore/${famID}-01/binning_100k/haplotype"
 
 #rm -rf $dir/0-kmers
 
@@ -32,6 +32,6 @@ mv ${i}/assembly.fasta ${famID}_flye_${i}_ctg.fasta
 N50 ${famID}_flye_${i}_ctg.fasta ${famID}_flye_${i}_ctg.n50 10000
 yak trioeval ${yak2} ${yak3} ${famID}_flye_${i}_ctg.fasta -t ${threads} > ${famID}_flye_${i}_ctg.ye
 echo "print Syntent for ${i}!"
-sh /slurm/home/zju/zhanglab/wudongya/software/asm2ref/unimap/run_unimap_dotplot.sh /slurm/home/zju/zhanglab/wudongya/APG/reference/CHM13v2m.fasta ${famID}_flye_${i}_ctg.fasta ${famID}_flye_${i}_ctg
+sh /YOUR_PATH/software/asm2ref/unimap/run_unimap_dotplot.sh /YOUR_PATH/reference/CHM13v2m.fasta ${famID}_flye_${i}_ctg.fasta ${famID}_flye_${i}_ctg
 done
 
