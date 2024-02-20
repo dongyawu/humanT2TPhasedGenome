4253cb703ca1dcc2768721ea7c36fc402a711cab
Dongya Wu <31070209+dongyawu@users.noreply.github.com>
2024-02-20 15:06:50 +0800

    Update 03_hifi_filt_stat.sh

--- 03_hifi_filt_stat.sh
+++ 03_hifi_filt_stat.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
-#SBATCH --job-name=C081
-#SBATCH --partition all
+#SBATCH --job-name=CCSfilt
+#SBATCH --partition=cpu64
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=60
+#SBATCH --cpus-per-task=20
 #SBATCH --mem=250g
 #SBATCH --time=900:00:00
 
 date
 
-ID="C081-CHA-C01-01"
-threads=60
+ID="XX"
+threads=20
 
-###Note the bam file suffix!!!
+###Note the bam file suffix and directory!!!
 mv */*hifi_reads.bam* ./
 mv */*/*hifi_reads.bam* ./
 
 ls *.hifi_reads.bam|while read a
 do
 	pre=`echo $a|sed 's/.hifi_reads.bam//g'`
+ 	##https://github.com/sheinasim/HiFiAdapterFilt
 	hifiadapterfilt.sh -l 44 -m 97 -t ${threads} -p $pre
 	rm -f ${pre}.hifi_reads.contaminant.blastout
 done
 
 list=`ls *filt.fastq.gz | xargs`
-zcat $list | ~/anaconda3/bin/seqkit stats -aT -j ${threads} -t dna > $ID\_hifi.stat
+zcat $list | seqkit stats -aT -j ${threads} -t dna > $ID\_hifi.stat
 
+echo "HiFi filtering finished..."
