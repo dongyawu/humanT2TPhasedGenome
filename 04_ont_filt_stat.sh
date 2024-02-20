6490a7aa3ef161c5a1c40ca4e57296bedf053d4c
Dongya Wu <31070209+dongyawu@users.noreply.github.com>
2024-02-20 15:10:30 +0800

    Update 04_ont_filt_stat.sh

--- 04_ont_filt_stat.sh
+++ 04_ont_filt_stat.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-#SBATCH --job-name=ontC081
-#SBATCH --partition all
+#SBATCH --job-name=ONTfilt
+#SBATCH --partition=cpu64
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=50
+#SBATCH --cpus-per-task=30
 #SBATCH --mem=200g
 #SBATCH --time=900:00:00
 
 date
 
-ID="C081-CHA-C01-01"
-threads=50
+ID="XXX"
+threads=30
 
 ###Note the file suffix
 mv */*pass.fastq.gz ./
@@ -25,6 +25,7 @@ do
         gunzip -c $pre\.pass.fastq.gz | ~/anaconda3/bin/NanoFilt -l 100000 | gzip > $pre.pass_100k.fastq.gz
 done
 
+###If some fastq files have same names but from different sequencing cells, mv will not work
 cat slurm* | grep "over" | cut -f8 -d" " | perl -npe "s/‘//" | perl -npe "s/’//" > left_bam
 i=1
 for bam in `cat left_bam`
@@ -36,7 +37,9 @@ do
         let i+=1
 done
 
-nanofq=`ls *pass_100k.fastq.gz |xargs`
-~/anaconda3/bin/NanoStat --fastq ${nanofq} -t ${threads} --tsv -o ./ > ${ID}_ont100k.stat
-###seqkit is Alternative
+##Statistic
+nanofq100k=`ls *pass_100k.fastq.gz |xargs`
+~/anaconda3/bin/NanoStat --fastq ${nanofq100k} -t ${threads} --tsv -o ./ > ${ID}_ont100k.stat
+nanofq=`ls *pass.fastq.gz |xargs`
+~/anaconda3/bin/NanoStat --fastq ${nanofq} -t ${threads} --tsv -o ./ > ${ID}_ont.stat
 
