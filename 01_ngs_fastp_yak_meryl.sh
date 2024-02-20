ed3528043735fb34829427ff31d7db010853b4d1
Dongya Wu <31070209+dongyawu@users.noreply.github.com>
2024-02-20 15:16:06 +0800

    Update 01_ngs_fastp_yak_meryl.sh

--- 01_ngs_fastp_yak_meryl.sh
+++ 01_ngs_fastp_yak_meryl.sh
@@ -9,7 +9,8 @@
 date
 
 threads=30
-ID=1
+#1: child; #2: paternal; #3: yak: maternal;
+ID = 1
 
 mv */*fq.gz ./
 
@@ -18,27 +19,48 @@ ls *_1.fq.gz|while read a
 do
         pre=`echo $a|sed 's/_1.fq.gz//g'`
         echo $pre
-        ~/software/anaconda/bin/fastp -i $pre\_1.fq.gz -o $pre\_1.clean.fq.gz -I $pre\_2.fq.gz -O $pre\_2.clean.fq.gz -j $pre.json -h $pre.html -u 30 -q 20 -w $threads
+        fastp -i $pre\_1.fq.gz -o $pre\_1.clean.fq.gz -I $pre\_2.fq.gz -O $pre\_2.clean.fq.gz -j $pre.json -h $pre.html -u 30 -q 20 -w $threads
 done
 echo "QC Finished!"
 
+###buiding kmer library for HIFIASM
 echo "Yak K-mer..."
 ################
 #1.yak offspring  #2.yak paternal  #3.yak maternal
-all=`ls *.clean.fq.gz`;
-~/software/anaconda/bin/yak count -k31 -b37 -o ${ID}.yak $all -t ${threads}
+yak count -k31 -b37 -o ${ID}.yak -t ${threads} <(zcat *.clean.fq.gz) <(zcat *.clean.fq.gz)
 echo "yak K-mer Finished!"
 
+###building kmer library for VERKKO
 echo "Meryl K-mer..."
-mem="200g"
+mem="250g"
 ls *.clean.fq.gz|while read a
 do
         pre=`echo $a|sed 's/.clean.fq.gz//g'`
-        /slurm/soft/meryl/1.3/bin/meryl k=30 threads=${threads} memory=$mem count compress output $pre\.meryl $pre\.clean.fq.gz
+        meryl k=30 threads=${threads} memory=$mem count compress output $pre\.meryl $pre\.clean.fq.gz
 done
-/slurm/soft/meryl/1.3/bin/meryl threads=${threads} memory=$mem union-sum output ${ID}\.merys *.meryl
+meryl threads=${threads} memory=$mem union-sum output ${ID}\.merys *.meryl
 rm -rf *.meryl
+mv ${ID}\.merys ${ID}\.meryl
 echo "Meryl K-mer Finished!"
+
+###To confirm the trio kinship
+####################
+###Mapping to CHM13v2
+####################
+echo "downsaple to 30X"
+zcat *_1.clean.fq.gz | seqkit head -n 500000000 | gzip > $sample\_33X_R1.fq.gz
+zcat *_2.clean.fq.gz | seqkit head -n 500000000 | gzip > $sample\_33X_R2.fq.gz
+ref_genome="/YOUR_PATH/CHM13v2m.fasta"
+RG="@RG\tID:${sample}\tSM:${sample}\tLB:LIB\tPL:BGI"
+echo "33XDS mapping to CHM13"
+bwa mem $ref_genome $sample\_33X_R1.fq.gz $sample\_33X_R2.fq.gz -t $threads -R "$RG" > $sample\.sam
+samtools view -@ $threads -bS $sample\.sam > $sample\.bam
+samtools sort -@ $threads $sample\.bam -o $sample\.sorted.bam
+rm -f $sample\.bam
+rm -f $sample\.sam
+samtools index -@ $threads $sample\.sorted.bam
+samtools flagstat -@ $threads $sample\.sorted.bam > $sample\.flagstat
+echo "Mapping and sorting Finished!"
 
 echo "Finished!"
 date
